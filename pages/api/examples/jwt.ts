93d88d04d9a2086dec33acc38b43eeb0a3ee6a48
schwaneberg <Timon.Schwaneberg@adesso.de>
2024-03-14 09:29:09 +0100

    auth working

---
+++ pages/api/examples/jwt.ts
@@ -0,0 +1,7 @@
+import { getToken } from "next-auth/jwt"
+import type { NextApiRequest, NextApiResponse } from "next"
+
+export default async function jwt(req: NextApiRequest, res: NextApiResponse) {
+    const token = await getToken({ req })
+    res.send(JSON.stringify(token, null, 2))
+}
